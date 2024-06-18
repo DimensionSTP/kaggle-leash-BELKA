9c048c78dd73db9b66899749d0b74527033efcac
DimensionSTP <ddang8jh@gmail.com>
2024-06-19 07:40:38 +0900

    feat: load model as sequence classification, delete dropout and linear, forward return huggingface style output

--- src/architectures/models/huggingface_model.py
+++ src/architectures/models/huggingface_model.py
@@ -4,46 +4,26 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
-from transformers import AutoModel
+from transformers import AutoModelForSequenceClassification
 
 
 class HuggingFaceModel(nn.Module):
     def __init__(
         self,
         pretrained_model_name: str,
-        dropout_ratio: float,
         num_labels: int,
     ) -> None:
         super().__init__()
-        self.model = AutoModel.from_pretrained(
+        self.model = AutoModelForSequenceClassification.from_pretrained(
             pretrained_model_name,
+            num_labels=num_labels,
             output_hidden_states=False,
-        )
-        self.dropout = nn.Dropout(
-            p=dropout_ratio,
-        )
-        self.classifier = nn.Linear(
-            self.model.config.hidden_size,
-            num_labels,
+            ignore_mismatched_sizes=True,
         )
 
     def forward(
         self,
         encoded: Dict[str, torch.Tensor],
     ) -> Dict[str, torch.Tensor]:
-        label = encoded["labels"]
-        del encoded["labels"]
         output = self.model(**encoded)
-        logit = self.classifier(
-            self.dropout(
-                output.pooler_output,
-            )
-        )
-        loss = F.binary_cross_entropy_with_logits(
-            logit,
-            label,
-        )
-        return {
-            "logit": logit,
-            "loss": loss,
-        }
+        return output
