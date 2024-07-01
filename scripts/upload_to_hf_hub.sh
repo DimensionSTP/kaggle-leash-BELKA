ca6203cb2352d33b2cb79f81a472f61e61bbceda
DimensionSTP <ddang8jh@gmail.com>
2024-07-02 08:04:40 +0900

    add: postprocessing scripts

---
+++ scripts/upload_to_hf_hub.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+path="src/postprocessing"
+is_tuned="untuned"
+strategy="ddp"
+upload_user="DeepChem"
+model_type="ChemBERTa-77M-MLM"
+precision=32
+batch_size=1024
+epoch=10
+
+python $path/upload_to_hf_hub.py \
+    is_tuned=$is_tuned \
+    strategy=$strategy \
+    upload_user=$upload_user \
+    model_type=$model_type \
+    precision=$precision \
+    batch_size=$batch_size \
+    epoch=$epoch
