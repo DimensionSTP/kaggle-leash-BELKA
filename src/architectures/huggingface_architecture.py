9124af318a9f7a9944e8e9c2eabd8460c406bfb0
DimensionSTP <ddang8jh@gmail.com>
2024-07-20 06:49:37 +0900

    feat: adapt weight decay at optimizers

--- src/architectures/huggingface_architecture.py
+++ src/architectures/huggingface_architecture.py
@@ -18,6 +18,7 @@ class HuggingFaceArchitecture(LightningModule):
         average: str,
         strategy: str,
         lr: float,
+        weight_decay: float,
         period: int,
         eta_min: float,
         interval: str,
@@ -26,6 +27,7 @@ class HuggingFaceArchitecture(LightningModule):
         self.model = model
         self.strategy = strategy
         self.lr = lr
+        self.weight_decay = weight_decay
         self.period = period
         self.eta_min = eta_min
         self.interval = interval
@@ -93,6 +95,7 @@ class HuggingFaceArchitecture(LightningModule):
             optimizer = FusedAdam(
                 self.parameters(),
                 lr=self.lr,
+                weight_decay=self.weight_decay,
             )
         elif (
             self.strategy == "deepspeed_stage_2_offload"
@@ -101,11 +104,13 @@ class HuggingFaceArchitecture(LightningModule):
             optimizer = DeepSpeedCPUAdam(
                 self.parameters(),
                 lr=self.lr,
+                weight_decay=self.weight_decay,
             )
         else:
             optimizer = optim.AdamW(
                 self.parameters(),
                 lr=self.lr,
+                weight_decay=self.weight_decay,
             )
         t_max = self.period * self.trainer.num_training_batches
         scheduler = optim.lr_scheduler.CosineAnnealingLR(
