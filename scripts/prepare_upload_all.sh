1cd2f513941f8f4cc0dea82943520ba34fc11042
DimensionSTP <ddang8jh@gmail.com>
2024-12-24 18:37:06 +0900

    add: prepare and upload all scripts

---
+++ scripts/prepare_upload_all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+path="src/postprocessing"
+is_tuned="untuned"
+strategy="ddp"
+upload_user="DeepChem"
+model_type="ChemBERTa-77M-MLM"
+precision=32
+batch_size=1024
+model_detail="ChemBERTa-77M-MLM"
+
+python $path/prepare_upload_all.py \
+    is_tuned=$is_tuned \
+    strategy=$strategy \
+    upload_user=$upload_user \
+    model_type=$model_type \
+    precision=$precision \
+    batch_size=$batch_size \
+    model_detail=$model_detail
